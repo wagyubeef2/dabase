641664c9d6208416e16e8af082446eaaa92c4a64
wagyubeef2 <118259362+wagyubeef2@users.noreply.github.com>
2022-11-29 18:23:38 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <script src="https://kit.fontawesome.com/c8e4d183c2.js" crossorigin="anonymous"></script>   
+</head>
+ 
+<body>
+    <section>
+        <!--- navigation --->
+        <nav>
+            <!--- logo --->
+            <a href="#" class="logo">PT PLN Persero</a>
+            <!--- menu --->
+            <ul>
+                <li><a href="login.php">Sign In</a></li>
+                <li><a href="register.php">Sign Up</a></li>
+            </ul>
+        </nav>
+ 
+        <!--- text --->
+        <div class="text-container">
+            <p>Welcome To,</p>
+            <p>PT PLN PERSERO</p>
+            <p></p>
+            <button class="hire-btn"><a href="login.php">LOGIN</a></button>
+            <button class="down-cv"><a href="register.php">REGISTER</a></button>
+            <p>Menjadi Perusahaan Listrik Terkemuka se-Asia Tenggara dan #1 Pilihan Pelanggan untuk Solusi Energi.</p>
+        </div>
+        <!-- model -->
+        <img alt="model" class="model" src="1.png">  
+    </section>
+ 
+    <!-- footer -->
+    <footer>
+        <p>Apakah mau mengobrol dengan saya?</p>
+             
+        <!--paragraph-->
+        <p>Ini adalah beberapa akun media sosial saya,anda dapat menghubunginya.
+            Angel Paulina Siahaan
+            angel2131196@itpln.ac.id
+            (202131196) 
+            Helmy Prayoga
+            Helmy2131142@itpln.ac.id
+            (202131142)
+            Dwi Humira Fazizah Faturahma 
+            dwi2131098@itpln.ac.id
+            (202131098)
+            Muhammad Iqbal
+            (202131131)
+            iqbal2131131@itpln.ac.id
+        </p>
+        <!--copyright-->
+        <p class="copyright">Copyright by 
+        Kelompok Terserah</p>
+    </footer>
+ 
+    <!--social-attach-bar-->
+    <div class="social">
+        <a href="#"><i class="fab fa-whatsapp"></i></a>
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+        <a href="#"><i class="fab fa-youtube"></i></a>
+    </div>
+</body>
+ 
+</html>
